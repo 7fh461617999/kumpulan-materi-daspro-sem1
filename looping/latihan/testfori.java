7aa231d3f5d505f70bebe55816011bdd5fa98540
habibi <habibifirman117@gmail.com>
2025-10-20 10:02:00 +0700

    modifikasi sedikit

--- looping/latihan/testfori.java
+++ looping/latihan/testfori.java
@@ -5,13 +5,21 @@ import java.util.Scanner;
 public class testfori {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("masukan perputaran");
+        System.out.print("masukan jumlahh matkul : ");
         int perputaran = sc.nextInt();
+        int nilai =0;
         // (1. Inisialisasi) ; (2. Kondisi) ; (3. Iterasi)
         for (int i = 1; i <= perputaran; i++) {
             // 4. Blok Kode
-            System.out.println("Hello, putaran ke-" + i);
+            System.out.println("nilai/gpa matkul ke-" + i +": ");
+            int x = sc.nextInt();
+            nilai += x ;
+            
         }
+        System.out.println("============================================================================");
+            double nilaiakhir = nilai/perputaran;
+            System.out.println("nilai akhir/gpa : "+nilaiakhir);
+        
         sc.close();
     }
 }
--- looping/latihan/testnestedloop.java
+++ looping/latihan/testnestedloop.java
@@ -8,11 +8,20 @@ public class testnestedloop {
         System.out.print("input loop :");
         int loop = sc.nextInt();
         // Loop Luar (mengatur Baris, ke bawah)
-        for (int i = 1; i <= loop; i++) {
-
+        for (int i = 1;  i <= loop ; i++) {
             // Loop Dalam (mengatur Kolom, ke samping)
-            for (int j = 1; j <= loop; j++) {
-                System.out.print("*  "); // print() (tanpa 'ln') agar menyamping
+            for (int j = 1; j <= loop  ; j++) {
+                // if (((j+i)%2)==0) {
+                 System.out.print("*  "); // print() (tanpa 'ln') agar menyamping
+                // } else {
+                //     System.out.print("   ");
+                // }
+                // *     *     *     
+                //    *     *     *
+                // *     *     *
+                //    *     *     *  
+                // *     *     *
+                //    *     *     *  
             }
             // Pindah baris setelah 1 baris kolom selesai
             System.out.println();
